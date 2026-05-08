64e655ff84b67c1bf1a5e194dc7bde0de5d8b79d
KnightMiner <knightminer4@gmail.com>
2026-05-08 15:18:36 -0400

    Updated default config
    Based on 85820197694a1773fd2ef25dabd3ab07c3a19fe0 and 96de074d0feeb974298e56425b8bea282604c51f
    
    (cherry picked from commit 765211cce02f0d28f92db3710ca952a4d23270b5)

--- src/main/java/knightminer/tagstacker/Matcher.java
+++ src/main/java/knightminer/tagstacker/Matcher.java
@@ -53,13 +53,17 @@ public class Matcher {
         COMMON + "ingots/",
         COMMON + "nuggets/",
         COMMON + "storage_blocks/",
-        COMMON + "raw_materials/"
+        COMMON + "raw_materials/",
+        COMMON + "dusts/",
+        COMMON + "gems/"
       ), VALID_RESOURCE_LOCATION);
 
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST = BUILDER
       .comment("List of tags to ignore despite them matching one of the prefixes above.",
         "Format is 'domain:name', for example 'c:ingots/special' if `c:ingots/` is a prefix")
-      .defineListAllowEmpty("items.prefix_blacklist", List.of(), VALID_RESOURCE_LOCATION);
+      .defineListAllowEmpty("items.prefix_blacklist", List.of(
+        COMMON + "nuggets/brass_like" // from FTB materials
+      ), VALID_RESOURCE_LOCATION);
 
     public static final ForgeConfigSpec.BooleanValue SHOW_ICON = CLIENT_BUILDER
       .comment("If true, shows an icon on items which support tag stacking")
